f01c3cc8c55f45c29e11d6408d501d7444629d5b
Adricf28 <adricf2807@gmail.com>
2023-11-04 21:38:50 +0100

    arreglo strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: adcarmon <adcarmon@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 18:55:59 by adcarmon          #+#    #+#             */
-/*   Updated: 2023/11/04 21:18:32 by adcarmon         ###   ########.fr       */
+/*   Updated: 2023/11/04 21:37:04 by adcarmon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,13 @@
 
 char	*ft_strdup(const char *str)
 {
-	int	slen;
 	char	*ptr;
 	int	i;
 
-	slen = ft_strlen(str) + 1;
-	ptr = (char *) malloc(slen * sizeof(char));
-	i = -1;
+	ptr = (char *) malloc((ft_strlen(str) + 1) * sizeof(char));
 	if (ptr == NULL)
-		return (NULL);
+		return (ptr);
+	i = -1;
 	while (str[++i])
 		ptr[i] = str[i];
 	ptr[i] = '\0';
